ba555f2ad36a2a012d9f4bea7be16b2739811366
bernardogoltz <78585520+bernardogoltz@users.noreply.github.com>
2022-04-01 15:27:56 -0300

    aA
    a

---
+++ sorting/selectionSort.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 11:57:57 2022
+
+@author: angel
+"""
+
---
+++ sorting/simple-sort.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 11:33:49 2022
+
+@author: bernardogoltz
+"""
+
+import numpy as np 
+
+np.random.seed(10)
+elementos = np.random.randint(10,100,100)
+
+maior = 0 
+
+for i in range(len(elementos)):
+    if elementos[i] > elementos[maior]:
+        maior = i
+    
+print("O maior é o {}, que é {}".format(maior,elementos[maior]))
+
